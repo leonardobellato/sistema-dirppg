4b8acc65b39bf6cc6a7599350902d1299a2030e3
dycaels <dycaels.github@gmail.com>
2025-09-02 13:58:29 -0300

    att bd

--- dirppg_inscricao.sql
+++ dirppg_inscricao.sql
@@ -22,7 +22,7 @@ CREATE TABLE `areas_concentracao` (
   `id_area_concentracao` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   `id_curso`             INT UNSIGNED NOT NULL,
   `nome`                 VARCHAR(150) NOT NULL,
-  `ativo`                TINYINT(1) NOT NULL DEFAULT 1
+  `inativa`              TINYINT(1) NOT NULL DEFAULT 0
 );
 
 ALTER TABLE `areas_concentracao` 
@@ -37,7 +37,7 @@ CREATE TABLE `linhas_pesquisa` (
   `id_linha_pesquisa`    INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   `id_area_concentracao` INT UNSIGNED NOT NULL,
   `nome`                 VARCHAR(150) NOT NULL,
-  `ativo`                TINYINT(1) NOT NULL DEFAULT 1
+  `inativa`              TINYINT(1) NOT NULL DEFAULT 0
 );
 
 ALTER TABLE `linhas_pesquisa`
@@ -52,7 +52,7 @@ CREATE TABLE `sublinhas` (
   `id_sublinha`       INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   `id_linha_pesquisa` INT UNSIGNED NOT NULL,
   `nome`              VARCHAR(150) NOT NULL,
-  `ativo`             TINYINT(1) NOT NULL DEFAULT 1
+  `inativa`           TINYINT(1) NOT NULL DEFAULT 0
 );
 
 ALTER TABLE `sublinhas`
@@ -67,7 +67,7 @@ CREATE TABLE `disciplinas` (
   `id_disciplina` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   `id_curso`      INT UNSIGNED NOT NULL,
   `nome`          VARCHAR(200) NOT NULL,
-  `ativo`         TINYINT(1) NOT NULL DEFAULT 1
+  `inativa`       TINYINT(1) NOT NULL DEFAULT 0
 );
 
 ALTER TABLE `disciplinas`
@@ -102,7 +102,7 @@ ALTER TABLE `candidatos`
 
 
 CREATE TABLE `professor_programa` (
-  `id_usuario` INT UNSIGNED NOT NULL,
+  `id_usuario`  INT UNSIGNED NOT NULL,
   `id_programa` INT UNSIGNED NOT NULL
 );
 
@@ -121,36 +121,110 @@ ALTER TABLE `professor_programa`
   ON UPDATE CASCADE;
 
 
-
-
-
-
-
-
-
-CREATE TABLE `edital` (
-  `id_edital` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  `id_curso` INT UNSIGNED NOT NULL,
-  `nome` VARCHAR(200) NOT NULL,
-  `vigente` TINYINT(1) DEFAULT 1,
-  `data_inscricao_inicio` DATE NOT NULL,
-  `data_inscricao_fim` DATE NOT NULL,
-  `data_recurso1_inicio` DATE NOT NULL,
-  `data_recurso1_fim` DATE NOT NULL,
-  `data_homologacao1_inicio` DATE NOT NULL,
-  `data_homologacao1_fim` DATE NOT NULL,
-  `data_recurso2_inicio` DATE DEFAULT NULL,
-  `data_recurso2_fim` DATE DEFAULT NULL,
-  `data_homologacao2_inicio` DATE DEFAULT NULL,
-  `data_homologacao2_fim` DATE DEFAULT NULL
+CREATE TABLE `editais` (
+  `id_edital`                 INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  `id_curso`                  INT UNSIGNED NOT NULL,
+  `nome`                      VARCHAR(200) NOT NULL,
+  `vigente`                   TINYINT(1) NOT NULL DEFAULT 1,
+  `data_inscricao_inicio`     DATE NOT NULL,
+  `data_inscricao_fim`        DATE NOT NULL,
+  `data_recurso1_inicio`      DATE NOT NULL,
+  `data_recurso1_fim`         DATE NOT NULL,
+  `data_homologacao1_inicio`  DATE NOT NULL,
+  `data_homologacao1_fim`     DATE NOT NULL,
+  `data_recurso2_inicio`      DATE NOT NULL,
+  `data_recurso2_fim`         DATE NOT NULL,
+  `data_homologacao2_inicio`  DATE NOT NULL,
+  `data_homologacao2_fim`     DATE NOT NULL
 );
 
-ALTER TABLE `edital` 
+ALTER TABLE `editais` 
   ADD CONSTRAINT fk_edital_curso 
   FOREIGN KEY (`id_curso`) 
-  REFERENCES `curso`(`id_curso`)
+  REFERENCES `cursos`(`id_curso`)
   ON DELETE CASCADE
   ON UPDATE CASCADE;
+
+
+CREATE TABLE `inscricoes` (
+  `id_inscricao`         INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  `id_usuario`           INT UNSIGNED NOT NULL,
+  `id_edital`            INT UNSIGNED NOT NULL,
+  `id_linha_pesquisa`    INT UNSIGNED NULL,
+  `id_sublinha`          INT UNSIGNED NULL,
+  `deferido`             TINYINT(1) DEFAULT NULL,
+  `motivo_indeferimento` VARCHAR(600) DEFAULT NULL,
+  `data_criacao`         DATE DEFAULT CURRENT_DATE,
+  `nome_orientador`      VARCHAR(100) DEFAULT NULL,
+  `observacao`           VARCHAR(600) DEFAULT NULL
+);
+
+ALTER TABLE `inscricoes` 
+  ADD CONSTRAINT fk_inscricao_usuario 
+  FOREIGN KEY (`id_usuario`) 
+  REFERENCES `usuarios`(`id_usuario`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+ALTER TABLE `inscricoes` 
+  ADD CONSTRAINT fk_inscricao_edital 
+  FOREIGN KEY (`id_edital`) 
+  REFERENCES `editais`(`id_edital`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+ALTER TABLE `inscricoes` 
+  ADD CONSTRAINT fk_inscricao_linha_pesquisa 
+  FOREIGN KEY (`id_linha_pesquisa`) 
+  REFERENCES `linhas_pesquisa`(`id_linha_pesquisa`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+ALTER TABLE `inscricoes` 
+  ADD CONSTRAINT fk_inscricao_sublinhas 
+  FOREIGN KEY (`id_sublinha`) 
+  REFERENCES `sublinhas`(`id_sublinha`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+
+CREATE TABLE `inscricao_disciplina` (
+  `id_inscricao`         INT UNSIGNED NOT NULL,
+  `id_disciplina`        INT UNSIGNED NOT NULL,
+  `deferido`             TINYINT(1) DEFAULT NULL,
+  `motivo_indeferimento` VARCHAR(600) DEFAULT NULL,
+);
+
+ALTER TABLE `inscricao_disciplina` 
+  ADD CONSTRAINT fk_isd_inscricao
+  FOREIGN KEY (`id_inscricao`) 
+  REFERENCES `inscricoes`(`id_inscricao`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+ALTER TABLE `inscricao_disciplina` 
+  ADD CONSTRAINT fk_isd_disciplina
+  FOREIGN KEY (`id_disciplina`) 
+  REFERENCES `disciplinas`(`id_disciplina`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+
+CREATE TABLE `documento` (
+  `id_documento`       INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  `id_inscricao`       INT UNSIGNED NOT NULL,
+  `caminho_servidor`   VARCHAR(200) DEFAULT NULL,
+  `tipo`               VARCHAR(30) NOT NULL,
+  
+);
+
+ALTER TABLE `documento` 
+  ADD CONSTRAINT fk_documento_inscricao
+  FOREIGN KEY (`id_inscricao`) 
+  REFERENCES `inscricoes`(`id_inscricao`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
 
 
 
@@ -219,38 +293,6 @@ CREATE TABLE `documento` (
 
 
 
-CREATE TABLE `inscricao` (
-  `cod_inscricao` int(11) NOT NULL,
-  `cod_candidato` int(11) DEFAULT NULL,
-  `cod_edital` int(11) DEFAULT NULL,
-  `cod_linha_pes` int(11) DEFAULT NULL,
-  `cod_sublinha` int(11) DEFAULT NULL,
-  `mensagem` VARCHAR(600) DEFAULT NULL,
-  `status_inscricao` VARCHAR(1) DEFAULT NULL,
-  `data_inscricao` date DEFAULT NULL,
-  `situacao_cand_programa` VARCHAR(1) DEFAULT NULL,
-  `coorientador` VARCHAR(200) DEFAULT NULL,
-  `orientador` VARCHAR(200) DEFAULT NULL,
-  `observacao` VARCHAR(600) DEFAULT NULL,
-  `recurso_primeira_fase` VARCHAR(200) DEFAULT NULL,
-  `recurso_segunda_fase` VARCHAR(200) DEFAULT NULL,
-  `recurso_terceira_fase` VARCHAR(200) DEFAULT NULL,
-  `recurso_quarta_fase` VARCHAR(200) DEFAULT NULL,
-  `motivo_indeferimento` VARCHAR(2000) DEFAULT NULL,
-  `motivo_indeferimento_recurso_primeira_fase` VARCHAR(2000) DEFAULT NULL,
-  `motivo_indeferimento_recurso_segunda_fase` VARCHAR(2000) DEFAULT NULL,
-  `motivo_indeferimento_recurso_terceira_fase` VARCHAR(2000) DEFAULT NULL,
-  `motivo_indeferimento_recurso_quarta_fase` VARCHAR(2000) DEFAULT NULL,
-  `status_recurso_primeira_fase` int(11) DEFAULT NULL,
-  `status_recurso_segunda_fase` int(11) DEFAULT NULL,
-  `status_recurso_terceira_fase` int(11) DEFAULT NULL,
-  `status_recurso_quarta_fase` int(11) DEFAULT NULL
-);
 
 
-CREATE TABLE `inscricao_disciplina` (
-  `cod_inscricao` int(11) NOT NULL,
-  `cod_disciplina` bigint(20) NOT NULL,
-  `motivo_indeferimento` VARCHAR(2000) DEFAULT NULL,
-  `status_inscricao` VARCHAR(1) DEFAULT NULL
-);
+
