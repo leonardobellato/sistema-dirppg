54389127aaa04390f6fc34c74efef4441c23970b
Repositorio DIRPPG PG <repositorio.dirppg.pg@gmail.com>
2025-10-16 11:37:46 -0300

    inscricao

--- app/Http/Controllers/InscricaoController.php
+++ app/Http/Controllers/InscricaoController.php
@@ -4,14 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Inscricao;
+use App\Models\Edital;
 
 class InscricaoController extends Controller
 {
-    // Método para listar objetos
-    public function index()
-    {
-        $editais = Inscricao::with(['curso.programa', 'fasesEdital'])->get();
 
-        return view('admin.editais.index', ['editais' => $editais]);
+    // Método para listar objetos
+    public function index($idCandidato)
+    {
+        $inscricoes = Inscricao::with('edital.curso.programa')->where('id_candidato', $idCandidato)->firstOrFail();
+
+        return view('candidato.inscricoes.index', ['inscricoes' => $inscricoes]);
+    }
+
+    public function getInscricoesByEdital($idEdital)
+    {
+        return response()->json(Edital::findOrFail($idEdital)->inscricoes);
+    }
+
+    // Método para mostrar o formulário de criação
+    public function create($idEdital)
+    {
+        // aquiii - tipo edital??
+
+        $edital = Edital::with('curso.programa')->findOrFail($idEdital);
+        return view('admin.pos.areas-concentracao.adicionar', compact('edital'));
     }
 }
